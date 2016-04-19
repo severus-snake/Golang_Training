bd8817e3960a65fdff62e9779d2b3f0fc3c30422
gamkedo-eandrade <eandrade21@gmail.com>
2016-04-18 23:10:10 -0700

    update for April 18

--- 10_for-loop/06_for_continue/main.go
+++ 10_for-loop/06_for_continue/main.go
@@ -7,7 +7,8 @@ func main() {
 	for {
 		i++
 		if i%2 == 0 {
-			continue
+			continue //continue makes the loop go back to the top of the loop
+			// and ignores all the lines of the loop code below it
 		}
 		fmt.Println(i)
 		if i >= 50 {
