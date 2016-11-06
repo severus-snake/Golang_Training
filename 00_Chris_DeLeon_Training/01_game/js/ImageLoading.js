6d4e4aed737e3f8b4fed2e509e42c013132bb00a
Eric Andrade <eandrade21@gmail.com>
2016-11-06 15:21:28 -0800

    added file changes from section 13 of udemy training

--- 00_Chris_DeLeon_Training/01_game/js/ImageLoading.js
+++ 00_Chris_DeLeon_Training/01_game/js/ImageLoading.js
@@ -14,7 +14,7 @@ function countLoadedImagesAndLaunchIfReady(){
 
 function beginLoadingImage(imgVar, fileName){
     imgVar.onload = countLoadedImagesAndLaunchIfReady;
-    imgVar.src = fileName;
+    imgVar.src = "images/"+fileName;
 }
 
 function loadImages(){
